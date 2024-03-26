ebea10ce336856888c0677579e882d4e9ea726c9
xiaolintodu <164989609+xiaolintodu@users.noreply.github.com>
2024-03-26 16:32:47 +0800

    Update go_train.py

--- go_train.py
+++ go_train.py
@@ -1,5 +1,5 @@
 import os
-os.system("python train.py --data tt100k.yaml --cfg yolov5_GFPN.yaml --weights pretrained/yolov5s.pt --epoch 100 --batch-size 4")
+os.system("python /kaggle/working/IMYOLOV5/train.py --data tt100k.yaml --cfg yolov5_GFPN.yaml --weights pretrained/yolov5s.pt --epoch 100 --batch-size 4")
 #os.system("python train.py --data my_data.yaml --cfg yolov5m.yaml --weights pretrained/yolov5m.pt --epoch 100 --batch-size 4")
 #os.system("python train.py --data my_data.yaml --cfg yolov5l.yaml --weights pretrained/yolov5l.pt --epoch 100 --batch-size 4")
 
